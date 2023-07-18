b3597376f951737d2127dd90a9f18794bf150713
kadirovMuhammadziz7 <kadirovmuhammadaziz79@gmail.com>
2023-07-18 16:00:46 +0600

    fghjk

--- lib/features/medisicalApp/presentation/pages/apointment_page.dart
+++ lib/features/medisicalApp/presentation/pages/apointment_page.dart
@@ -32,9 +32,83 @@ class ApointmentPage extends StatelessWidget {
                         color: Colors.white,
                         size: 25,
                       ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.more_vert,
+                        color: Colors.white,
+                        size: 25,
+                      ),
                     )
                   ],
                 ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage:
+                            AssetImage('assets/images/doctor1.jpg'),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Dr. Doctor Name ',
+                        style: TextStyle(
+                            fontSize: 23,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Therapist',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                color: Color(0xff9F97E2),
+                                shape: BoxShape.circle),
+                            child: Icon(
+                              Icons.call,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                color: Color(0xff9F97E2),
+                                shape: BoxShape.circle),
+                            child: Icon(
+                              Icons.chat,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
               ]),
             )
           ],
